20129b51531fea6e16b783b210e8863e2cb36c43
Xyrox47 <xwatkins04@hotmail.com>
2025-09-21 11:26:03 -0400

    Problem 2 Working

--- src/arrays.ts
+++ src/arrays.ts
@@ -18,7 +18,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripleArray = numbers.map((num: number): number => 3 * num);
+    return tripleArray;
 }
 
 /**
