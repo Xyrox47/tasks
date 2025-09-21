f037a5589368954659cb01fd9e757191c6ea321c
Xyrox47 <xwatkins04@hotmail.com>
2025-09-21 11:45:41 -0400

    Problem 3 Complete

--- src/arrays.ts
+++ src/arrays.ts
@@ -27,7 +27,11 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const intoInt = numbers.map((str: string): number =>
+        isNaN(parseInt(str)) ? 0 : parseInt(str),
+    );
+
+    return intoInt;
 }
 
 /**
